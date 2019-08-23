6c8130837db6b9c5cdd9c85a9693577123af5474
andrewdavis23 <andrewdavis23@gmail.com>
2019-08-23 08:28:57 -0400

    add,commit,push

--- main.py
+++ main.py
@@ -7,39 +7,29 @@ option = -1
 steps = 1
 
 while option != 0:
-    print(' ______________________________________________________________ ')
-    print('|                                                              |')
-    print('| 1 - Create CSV of all 4-digit combos and Kaprekar step count |')
-    print('| 2 - Enter a number                                           |')
-    print('| 0 - EXIT                                                     |')
-    print('|                                                              |')
+    print(' _______________________________________________________________ ')
+    print('|                                                               |')
+    print('| 1 - Create CSV                                                |')
+    print('| 2 - Enter a number                                            |')
+    print('| 0 - EXIT                                                      |')
+    print('|                                                               |')
 
     option = int(input('| >'))
-    print('|______________________________________________________________|')
-
 
     if option == 1:
-        print(' ______________________________________________________________ ')
-        print('|                                                              |')
-        print('| Creating CSV...                                              |')
-        print('|                                                              |')
+        print('Creating CSV...')
 
         digits = ('0','1','2','3','4','5','6','7','8','9')
         x = []
         y = []
 
+        # Create all four digit combos in x
         for a in digits:
             for b in digits:
                 for c in digits:
                     for d in digits:
                         if not(a==b and a==c and a==d):
                             x.append(a+b+c+d)
-
-        # for i in x:
-        #     for j in x:
-        #         if ''.join(sorted(i)) == ''.join(sorted(j)) and i != j:
-        #             x.remove(i)
-        #             break
 
         # Remove permutations from x
         i = 0
@@ -49,7 +39,7 @@ while option != 0:
                     x.remove(x[i])
             i += 1 
 
-        # Create step count in y
+        # Create step count in y = stepCount(x)
         for i in x:
                 y.append(stepCount(i))
                 i = int(i)
@@ -67,41 +57,38 @@ while option != 0:
                 writer = csv.writer(csvFile)
                 writer.writerows(csvData)
     
-        print('| CSV created.                                                 |')
-        print('|______________________________________________________________|')
+        print('CSV created.')
         option = -1
 
 
     if option == 2:
-        ########ENTER ONE AT A TIME:
-        print(' ______________________________________________________________ ')
-        print('|                                                              |')
-        print('| Input a four digit number where all digits are not the same  |')
+        print('| Input a four digit number where all digits are not the same')
 
         option = 0
         while option == 0:
-            x = input('| >')
-            digits = int(math.log10(int(x))+1)
+            inNum = input('| >')
+            digits = int(math.log10(int(inNum))+1)
             if digits != 4:
                 print('| This is',digits,'digits. This number is needs to be 4 digits.')
 
             if digits != 1:
-                if x[0] == x[1] and x[1] == x[2] and x[2] == x[3]:
-                    print('| These digits are not unique')
+                if inNum[0] == inNum[1] and inNum[1] == inNum[2] and inNum[2] == inNum[3]:
+                    print('| These digits are all the same. At least one needs to be different.')
 
-            if digits == 4 and not(x[0] == x[1] and x[1] == x[2] and x[2] == x[3]):
+            if digits == 4 and not(inNum[0] == inNum[1] and inNum[1] == inNum[2] and inNum[2] == inNum[3]):
                 option = 2
   
         if option == 2:
-            result = kr(x)
-            print('|\t\t\t',steps,'\t',result,'\t\t\t|')
+            print('|\t\t\t','Step','\t','Result','\t\t\t|')
+            print('|\t\t\t','0','\t',inNum,'\t\t\t\t|')
+            result = kr(inNum)
+            print('|\t\t\t',steps,'\t',result,'\t\t\t\t|')
             while result != '6174':
                 result = kr(result)
                 steps = steps + 1
-                print('|\t\t\t',steps,'\t',result,'\t\t\t|')
+                print('|\t\t\t',steps,'\t',result,'\t\t\t\t|')
             steps = 1
         
-        print('|______________________________________________________________|')
         option = -1
 
 
